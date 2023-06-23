b5d8a8dab88962e9dec355ef9a7306fe2727d512
tratchapong <tratchapong@gmail.com>
2023-06-23 20:41:04 +0700

    Chat : chat ok without room

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
   res.send("This is chat Server..");
 });
 let users = [];
+let allMsg = []
 
 io.on("connection", (socket) => {
   console.log("connect : ", socket.id);
@@ -28,8 +29,16 @@ io.on("connection", (socket) => {
     console.log("Disconnect : ", socket.id);
     let idx = users.findIndex(el => el.id === socket.id)
     users.splice(idx, 1)
+    if(users.length === 0)
+      allMsg = []
     console.log(users)
   });
+  socket.on("sendMessage", msg => {
+    // console.log( socket.id,' : ', msg)
+    allMsg.push({id: socket.id, msg : msg})
+    console.log(allMsg)
+    io.emit("getMessage", allMsg)
+  })
 });
 
 server.listen(8080, () => console.log("Server on 8080..."));
