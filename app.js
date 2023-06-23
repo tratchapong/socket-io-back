7a16dc38094a244fb806ce71f8f479b9cb76de36
tratchapong <tratchapong@gmail.com>
2023-06-23 14:43:01 +0700

    Chat : user enter + leave

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server,{
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
   cors: {
-    origin: '*'
-  }
-})
+    origin: "*",
+  },
+});
 
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("This is chat Server..");
+});
+let users = [];
 
-app.use(cors())
-app.get('/', (req, res) => {
-  res.send('This is chat Server..')
-})
+io.on("connection", (socket) => {
+  console.log("connect : ", socket.id);
+  socket.on("enter", (username) => {
+    let isNameExist = users.findIndex( el => el.name === username) !== -1
+    if(!isNameExist)
+      users.push({id: socket.id, name: username});
+    console.log(users);
+  });
+  socket.on("disconnect", () => {
+    console.log("Disconnect : ", socket.id);
+    let idx = users.findIndex(el => el.id === socket.id)
+    users.splice(idx, 1)
+    console.log(users)
+  });
+});
 
-io.on('connection', socket => {
-  console.log('have a connected event')
-  socket.on('disconnect', socket => {
-    console.log('have a Disconnected event')
-  })
-})
-
-server.listen(8080, ()=> console.log('Server on 8080...'))
+server.listen(8080, () => console.log("Server on 8080..."));
